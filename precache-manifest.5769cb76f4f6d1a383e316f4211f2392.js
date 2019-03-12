244378da1053458fa417ac243f9e78f7d21db965
Aaron Cox <aaron@greymass.com>
2019-03-12 02:29:11 -0400

    Updates

---
+++ precache-manifest.5769cb76f4f6d1a383e316f4211f2392.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/eosio-uri-redirect/static/media/icons.674f50d2.eot"
+  },
+  {
+    "revision": "3a93ca72ec52be2e8e90",
+    "url": "/eosio-uri-redirect/static/css/main.776e9c3f.chunk.css"
+  },
+  {
+    "revision": "3aeb19ecb7c88ddc372a",
+    "url": "/eosio-uri-redirect/static/js/1.3aeb19ec.chunk.js"
+  },
+  {
+    "revision": "b21a81ac740e74e24157",
+    "url": "/eosio-uri-redirect/static/js/runtime~main.b21a81ac.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/eosio-uri-redirect/static/media/icons.af7ae505.woff2"
+  },
+  {
+    "revision": "3a93ca72ec52be2e8e90",
+    "url": "/eosio-uri-redirect/static/js/main.3a93ca72.chunk.js"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/eosio-uri-redirect/static/media/icons.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/eosio-uri-redirect/static/media/icons.912ec66d.svg"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/eosio-uri-redirect/static/media/icons.fee66e71.woff"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/eosio-uri-redirect/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "bbc0c781d5e493e81b0187af19384a2a",
+    "url": "/eosio-uri-redirect/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/greymass/eosio-uri-redirect/precache-manifest.6878411f31245de90f054fd5b9066a9a.js"
+  "/eosio-uri-redirect/precache-manifest.5769cb76f4f6d1a383e316f4211f2392.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/greymass/eosio-uri-redirect/index.html", {
+workbox.routing.registerNavigationRoute("/eosio-uri-redirect/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
