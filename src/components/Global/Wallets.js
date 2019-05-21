7515c960fa4241faf613b8a801a2df5aa1626ada
Aaron Cox <aaron@greymass.com>
2019-05-21 12:35:01 -0400

    Updated wallet section

--- src/components/Global/Wallets.js
+++ src/components/Global/Wallets.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Header, Segment } from 'semantic-ui-react';
+import { Card, Header, Image, Segment } from 'semantic-ui-react';
 
 class GlobalWallets extends Component {
   render() {
@@ -8,10 +8,29 @@ class GlobalWallets extends Component {
         <Header as="h2" size="large">
           EEP-7 Compatible Wallets
           <Header.Subheader>
-            The <a href="$">EEP-7 standard</a> is a communication layer any wallet can adopt. Encourage your favorite wallet developer to implement this standard or download one of the wallets below.
+            The <a href="https://github.com/greymass/EEPs/blob/eep-x/EEPS/eep-7.md">EEP-7 standard</a> is a communication layer any wallet can adopt. Encourage your favorite wallet developer to implement this standard or download one of the wallets below.
           </Header.Subheader>
         </Header>
-        <p>No wallets available yet - this protocol is still under development.</p>
+        <Card
+          as="a"
+          href="https://github.com/greymass/eos-voter/releases/tag/v0.9.0-anchor"
+          raised
+        >
+          <Image src="https://i.imgur.com/U972tzQ.png" />
+          <Card.Content>
+            <Card.Header>Anchor BETA</Card.Header>
+            <Card.Meta>
+              Requires v0.9.0+
+            </Card.Meta>
+          </Card.Content>
+          <Card.Content extra>
+            Maintained by
+            {' '}
+            <a href="https://greymass.com">
+              Greymass
+            </a>
+          </Card.Content>
+        </Card>
       </Segment>
     );
   }
