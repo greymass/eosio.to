0c2c4a9f894bdaf9190ec6169ae04c79f68bc5d4
Aaron Cox <aaron@greymass.com>
2020-03-31 13:39:10 -0400

    Added more chains and better filtering

--- src/containers/request.js
+++ src/containers/request.js
@@ -145,6 +145,12 @@ const chainAliases = [
   ['MEETONE','cfe6486a83bad4962f232d48003b1824ab5665c36778141034d75e57b956e422'], // 0x07
   ['INSIGHTS','b042025541e25a472bffde2d62edd457b7e70cee943412b1ea0f044f88591664'], // 0x08
   ['BEOS','b912d19a6abd2b1b05611ae5be473355d64d95aeff0c09bedc8c166cd6468fe4'], // 0x09
+  ['LYNX','b62febe5aadff3d5399090b9565cb420387d3c66f2ccd7c7ac1f532c4f50f573'], // no shortcut
+  ['WAX','1064487b3cd1a897ce03ae5b6a865651747e2e152090f99c1d19d44e01aea5a4'], // no shortcut
+  ['LYNXTESTNET','f11d5128e07177823924a07df63bf59fbd07e52c44bc77d16acc1c6e9d22d37b'], // no shortcut
+  ['WAXTESTNET','f16b1833c747c43682f4386fca9cbb327929334a762755ebec17f6f23c9b8a12'], // no shortcut
+  ['FIO', '21dcae42c0182200e93f954a074011f9048a7624c6fe81d3c9541a614a88bd1c'], // no shortcut
+  ['FIOTESTNET', 'b20901380af44ef59c5918439a1f9a41d83669020319a80574b804a5f95cbd7e'], // no shortcut
 ];
 
 const chainAPIs = {
@@ -155,8 +161,14 @@ const chainAPIs = {
   '73647cde120091e0a4b85bced2f3cfdb3041e266cbbe95cee59b73235a1b3b6f': 'https://api.worbli.io',
   'd5a3d18fbb3c084e3b1f3fa98c21014b5f3db536cc15d08f9f6479517c6a3d86': 'https://hapi.bos.eosrio.io',
   'cfe6486a83bad4962f232d48003b1824ab5665c36778141034d75e57b956e422': 'https://fullnode.meet.one',
-  'b042025541e25a472bffde2d62edd457b7e70cee943412b1ea0f044f88591664': 'https://ireland-history.insights.network',
+  'b042025541e25a472bffde2d62edd457b7e70cee943412b1ea0f044f88591664': 'https://instar.greymass.com',
   'b912d19a6abd2b1b05611ae5be473355d64d95aeff0c09bedc8c166cd6468fe4': 'https://api.beos.world',
+  'b62febe5aadff3d5399090b9565cb420387d3c66f2ccd7c7ac1f532c4f50f573': 'https://lynx.greymass.com',
+  '1064487b3cd1a897ce03ae5b6a865651747e2e152090f99c1d19d44e01aea5a4': 'https://wax.greymass.com',
+  'f11d5128e07177823924a07df63bf59fbd07e52c44bc77d16acc1c6e9d22d37b': 'https://lynxtestnet.greymass.com',
+  'f16b1833c747c43682f4386fca9cbb327929334a762755ebec17f6f23c9b8a12': 'https://waxtestnet.greymass.com',
+  '21dcae42c0182200e93f954a074011f9048a7624c6fe81d3c9541a614a88bd1c': 'https://fio.greymass.com',
+  'b20901380af44ef59c5918439a1f9a41d83669020319a80574b804a5f95cbd7e': 'https://fiotestnet.greymass.com',
 }
 
 class RequestContainer extends Component {
@@ -233,6 +245,14 @@ class RequestContainer extends Component {
       default: {
         return chainAliases[chainIdValue];
       }
+      case "chain_id": {
+        const filtered = chainAliases.filter(c => {
+          if (c.length === 1) return false;
+          return c.length > 1 && c[1].toLowerCase() === chainIdValue.toLowerCase()
+        })
+        return filtered[0];
+      }
+
     }
   }
   decode = async (uri = false) => {
