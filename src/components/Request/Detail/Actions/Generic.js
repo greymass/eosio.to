bd067b6bf6fd4a135bd94a0cbd092401f2afbfd7
Aaron Cox <aaron@greymass.com>
2020-04-01 13:10:57 -0400

    Replace placeholder values with explanation

--- src/components/Request/Detail/Actions/Generic.js
+++ src/components/Request/Detail/Actions/Generic.js
@@ -11,12 +11,17 @@ class ActionRequestDetailActionsGeneric extends Component {
     return (
       <Table definition key={idx}>
         <Table.Body>
-          {Object.keys(action).map((param) => (
-            <Table.Row key={param}>
-              <Table.Cell collapsing textAlign="right">{param}</Table.Cell>
-              <Table.Cell><pre>{JSON.stringify(action[param], null, 2)}</pre></Table.Cell>
-            </Table.Row>
-          ))}
+          {Object.keys(action).map((param) => {
+            let formatted = JSON.stringify(action[param], null, 2);
+            formatted = formatted.replace("............1", "[Signing Account: Actor]")
+            formatted = formatted.replace("............2", "[Signing Account: Permission]")
+            return (
+              <Table.Row key={param}>
+                <Table.Cell collapsing textAlign="right">{param}</Table.Cell>
+                <Table.Cell><pre>{formatted}</pre></Table.Cell>
+              </Table.Row>
+            )
+          })}
         </Table.Body>
       </Table>
     );
