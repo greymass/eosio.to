d11cb95d721ad806dd056fba7aac97f46fca4345
Aaron Cox <aaron@greymass.com>
2020-04-13 19:25:07 -0400

    Fixed chain aliases
    Order matters in this situation

--- src/containers/request.js
+++ src/containers/request.js
@@ -136,21 +136,22 @@ const opts = {
 
 const chainAliases = [
   ['RESERVED'], // 0x00
-  ['EOS','aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906'], // 0x01
-  ['TELOS','4667b205c6838ef70ff7988f6e8257e8be0e1284a2f59699054a018f743b1d11'], // 0x02
-  ['JUNGLE','038f4b0fc8ff18a4f0842a8f0564611f6e96e8535901dd45e43ac8691a1c4dca'], // 0x03
-  ['KYLIN','5fff1dae8dc8e2fc4d5b23b2c7665c97f9e9d8edf2b6485a86ba311c25639191'], // 0x04
-  ['WORBLI','73647cde120091e0a4b85bced2f3cfdb3041e266cbbe95cee59b73235a1b3b6f'], // 0x05
-  ['BOS','d5a3d18fbb3c084e3b1f3fa98c21014b5f3db536cc15d08f9f6479517c6a3d86'], // 0x06
-  ['MEETONE','cfe6486a83bad4962f232d48003b1824ab5665c36778141034d75e57b956e422'], // 0x07
-  ['INSIGHTS','b042025541e25a472bffde2d62edd457b7e70cee943412b1ea0f044f88591664'], // 0x08
-  ['BEOS','b912d19a6abd2b1b05611ae5be473355d64d95aeff0c09bedc8c166cd6468fe4'], // 0x09
+  ['EOS','aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906'], // 1
+  ['TELOS','4667b205c6838ef70ff7988f6e8257e8be0e1284a2f59699054a018f743b1d11'], // 2
+  ['JUNGLE','038f4b0fc8ff18a4f0842a8f0564611f6e96e8535901dd45e43ac8691a1c4dca'], // 3
+  ['KYLIN','5fff1dae8dc8e2fc4d5b23b2c7665c97f9e9d8edf2b6485a86ba311c25639191'], // 4
+  ['WORBLI','73647cde120091e0a4b85bced2f3cfdb3041e266cbbe95cee59b73235a1b3b6f'], // 5
+  ['BOS','d5a3d18fbb3c084e3b1f3fa98c21014b5f3db536cc15d08f9f6479517c6a3d86'], // 6
+  ['MEETONE','cfe6486a83bad4962f232d48003b1824ab5665c36778141034d75e57b956e422'], // 7
+  ['INSIGHTS','b042025541e25a472bffde2d62edd457b7e70cee943412b1ea0f044f88591664'], // 8
+  ['BEOS','b912d19a6abd2b1b05611ae5be473355d64d95aeff0c09bedc8c166cd6468fe4'], // 9
+  ['WAX','1064487b3cd1a897ce03ae5b6a865651747e2e152090f99c1d19d44e01aea5a4'], // 10
+  ['WAXTESTNET','f16b1833c747c43682f4386fca9cbb327929334a762755ebec17f6f23c9b8a12'], // 11
+  ['FIO', '21dcae42c0182200e93f954a074011f9048a7624c6fe81d3c9541a614a88bd1c'], // 12
+  ['FIOTESTNET', 'b20901380af44ef59c5918439a1f9a41d83669020319a80574b804a5f95cbd7e'], // 13
+  ['TELOSTESTNET', '1eaa0824707c8c16bd25145493bf062aecddfeb56c736f6ba6397f3195f33c9f'], // 14
   ['LYNX','b62febe5aadff3d5399090b9565cb420387d3c66f2ccd7c7ac1f532c4f50f573'], // no shortcut
-  ['WAX','1064487b3cd1a897ce03ae5b6a865651747e2e152090f99c1d19d44e01aea5a4'], // no shortcut
   ['LYNXTESTNET','f11d5128e07177823924a07df63bf59fbd07e52c44bc77d16acc1c6e9d22d37b'], // no shortcut
-  ['WAXTESTNET','f16b1833c747c43682f4386fca9cbb327929334a762755ebec17f6f23c9b8a12'], // no shortcut
-  ['FIO', '21dcae42c0182200e93f954a074011f9048a7624c6fe81d3c9541a614a88bd1c'], // no shortcut
-  ['FIOTESTNET', 'b20901380af44ef59c5918439a1f9a41d83669020319a80574b804a5f95cbd7e'], // no shortcut
 ];
 
 const chainAPIs = {
@@ -163,13 +164,15 @@ const chainAPIs = {
   'cfe6486a83bad4962f232d48003b1824ab5665c36778141034d75e57b956e422': 'https://fullnode.meet.one',
   'b042025541e25a472bffde2d62edd457b7e70cee943412b1ea0f044f88591664': 'https://instar.greymass.com',
   'b912d19a6abd2b1b05611ae5be473355d64d95aeff0c09bedc8c166cd6468fe4': 'https://api.beos.world',
-  'b62febe5aadff3d5399090b9565cb420387d3c66f2ccd7c7ac1f532c4f50f573': 'https://lynx.greymass.com',
   '1064487b3cd1a897ce03ae5b6a865651747e2e152090f99c1d19d44e01aea5a4': 'https://wax.greymass.com',
-  'f11d5128e07177823924a07df63bf59fbd07e52c44bc77d16acc1c6e9d22d37b': 'https://lynxtestnet.greymass.com',
   'f16b1833c747c43682f4386fca9cbb327929334a762755ebec17f6f23c9b8a12': 'https://waxtestnet.greymass.com',
   '21dcae42c0182200e93f954a074011f9048a7624c6fe81d3c9541a614a88bd1c': 'https://fio.greymass.com',
   'b20901380af44ef59c5918439a1f9a41d83669020319a80574b804a5f95cbd7e': 'https://fiotestnet.greymass.com',
+  '1eaa0824707c8c16bd25145493bf062aecddfeb56c736f6ba6397f3195f33c9f': 'https://testnet.eos.miami',
+  'b62febe5aadff3d5399090b9565cb420387d3c66f2ccd7c7ac1f532c4f50f573': 'https://lynx.greymass.com',
+  'f11d5128e07177823924a07df63bf59fbd07e52c44bc77d16acc1c6e9d22d37b': 'https://lynxtestnet.greymass.com',
 }
+
 
 class RequestContainer extends Component {
   constructor(props) {
