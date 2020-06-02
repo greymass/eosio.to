46535a601eed9ffc8d2fb5e95ce5199e1ad24442
Aaron Cox <aaron@greymass.com>
2020-06-02 19:30:50 -0400

    Added Jungle 3

--- src/containers/request.js
+++ src/containers/request.js
@@ -149,7 +149,8 @@ const chainAliases = [
   ['PROTON','384da888112027f0321850a169f737c33e53b388aad48b5adace4bab97f437e0'], // 11
   ['FIO', '21dcae42c0182200e93f954a074011f9048a7624c6fe81d3c9541a614a88bd1c'], // 12
   // NAMED WITHOUT ALIAS
-  ['PROTONTESTNET', '71ee83bcf52142d61019d95f9cc5427ba6a0d7ff8accd9e2088ae2abeaf3d3dd']
+  ['PROTONTESTNET', '71ee83bcf52142d61019d95f9cc5427ba6a0d7ff8accd9e2088ae2abeaf3d3dd'],
+  ['JUNGLE3', '2a02a0053e5a8cf73a56ba0fda11e4d92e0238a4a2aa74fccf46d5a910746840'],
 ];
 
 const chainAPIs = {
@@ -171,6 +172,7 @@ const chainAPIs = {
   'f11d5128e07177823924a07df63bf59fbd07e52c44bc77d16acc1c6e9d22d37b': 'https://lynxtestnet.greymass.com',
   '384da888112027f0321850a169f737c33e53b388aad48b5adace4bab97f437e0': 'https://proton.greymass.com',
   '71ee83bcf52142d61019d95f9cc5427ba6a0d7ff8accd9e2088ae2abeaf3d3dd': 'https://protontestnet.greymass.com',
+  '2a02a0053e5a8cf73a56ba0fda11e4d92e0238a4a2aa74fccf46d5a910746840': 'https://jungle3.greymass.com',
 }
 
 
